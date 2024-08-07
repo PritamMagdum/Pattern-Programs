db873a237385919b92fba4cefb1756e2f8c36be3
Pritam <pritammagdum24@gmail.com>
2024-08-07 08:10:24 +0530

    Pattern 3 added

---
+++ 3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+1
+12
+123
+1234
+12345
+
+*/
+int pattern3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int t;
+    cout << "Enter test cases count" << endl;
+    cin >> t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin >> n; 
+        pattern3(n);
+    }
+}
