2f81941395d15bbe43d8e260895f4cf3e2c05db6
Pritam <pritammagdum24@gmail.com>
2024-08-08 06:45:09 +0530

    Pattern 16 added

---
+++ 16.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+A
+BB
+CCC
+DDDD
+EEEEE
+
+*/
+
+void pattern15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j=0; j <= i; j++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cout << "Enter test cases count" << endl;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        pattern15(n);
+    }
+}
